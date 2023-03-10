18d2590fa2e430fb4d8c480bbbae345faba5f1b0
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-03-10 14:27:08 +0700

    Update bruh.py

--- bruh.py
+++ bruh.py
@@ -5,21 +5,8 @@ import sys
 import time
 import ntplib
 
-
-
-
 ser = serial.Serial('COM4', baudrate=115200)
 
-# def RequestTimefromNtp(addr='th.pool.ntp.org'):
-#     REF_TIME_1970 = 2208988800  # Reference time
-#     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#     data = b'\x1b' + 47 * b'\0'
-#     client.sendto(data, (addr, 123))
-#     data, address = client.recvfrom(1024)
-#     if data:
-#         t = struct.unpack('!12I', data)[10]
-#         t -= REF_TIME_1970
-#     return time.ctime(t), t
 
 asd = ntplib.NTPClient()
 while True:
